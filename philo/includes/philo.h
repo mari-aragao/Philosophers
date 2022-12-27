9b20fb061115d1b9dc0dce5c2562c2ec5df864a7
mari-aragao <mariazevedoaragao@gmail.com>
2022-12-27 19:38:44 -0300

    update

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -24,36 +24,31 @@
 # define THINK 	3
 # define DIE 	4
 
-typedef	struct	s_mutex
+typedef	struct	s_vars
 {
-	pthread_mutex_t	print;
-	pthread_mutex_t	*forks;
-	int		*arr_forks;
-}		t_mutex;
-
-typedef struct	s_philo
-{
-	
-	int		id;
 	int		total;
 	int		time_to_die;
 	int		time_to_eat;
 	int		time_to_sleep;
 	int		meals_to_make;
-	int		meal_counter;
-	int		stop;
+	pthread_mutex_t	print;
+	pthread_mutex_t	*forks;
+	int		*arr_fk;
+}		t_vars;
+
+typedef struct	s_philo
+{
 	pthread_t	th;
-	long int	start;
-	long int	actual_time;
+	int		id;
+	int		meal_cntr;
+	int		stop;
+	long int	st_time; //start time
+	long int	ac_time; //actual time
 	long int	last_meal;
+	t_vars		*vars;
 
 }		t_philo;
 
-typedef struct	s_all
-{
-	t_philo *ph;
-	t_mutex	*mutex;
-}		t_all;
 /*
  stop = 0 - ta de boa
  stop = 1 - algum filosofo morreu
@@ -67,20 +62,19 @@ int	ft_atoi(char *s);
 int	all_checks(int argc, char **argv);
 int	check_positive_number(char **argv);
 int	check_bigger_than_zero(char **argv);
-t_all	*init_all(int argc, char **argv);
+t_philo	*init_all(int argc, char **argv);
 t_philo	*init_philo(int argc, char **argv);
 t_philo	*init_parameters(int argc, char **argv, t_philo *philo, int total);
-t_mutex	*init_mutex(int total);
-int	init_threads(t_all *all);
-void	destroy_mutex(int total, t_mutex *mutex);
-void	destroy_philo(t_philo *ph);
+t_vars	*init_vars(int argc, char **argv, int total);
+t_vars	*init_mutex(int total);
+int	init_threads(t_philo *ph);
+void	destroy_all(int total, t_philo *vars);
 long int	get_time(void);
-int	print(t_all *all, int action);
-void	*routine(void *all);
-void	take_forks(t_all *all);
-void	eating(t_all *all);
-void	drop_forks(t_all *all);
+int	print(t_philo ph, int action);
+void	*routine(void *ph);
+void	take_forks(t_philo ph);
+void	eating(t_philo ph);
+void	drop_forks(t_philo ph);
 void	count_time(long int tm);
-
 
 #endif
