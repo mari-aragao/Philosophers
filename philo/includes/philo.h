6d40d062b8229c0d5ed33b0ab52d170bce419a2c
mari-aragao <mariazevedoaragao@gmail.com>
2022-12-26 22:42:27 -0300

    create t_all

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -18,30 +18,41 @@
 # include <sys/time.h>
 # include <stdio.h>
 
+# define FORK	0
+# define EAT	1
+# define SLEEP	2
+# define THINK 	3
+# define DIE 	4
+
 typedef	struct	s_mutex
 {
 	pthread_mutex_t	print;
-	pthread_mutex_t	forks;
+	pthread_mutex_t	*forks;
 }		t_mutex;
 
 typedef struct	s_philo
 {
 	
-	int		philo_id;
-	int		total_philos;
+	int		id;
+	int		total;
 	int		time_to_die;
 	int		time_to_eat;
 	int		time_to_sleep;
 	int		meals_to_make;
 	int		meal_counter;
 	int		stop;
-	int		*arr_forks;
-	pthread_t	philo;
+	pthread_t	th;
 	long int	start;
 	long int	actual_time;
 	long int	last_meal;
+
 }		t_philo;
 
+typedef struct	s_all
+{
+	t_philo *ph;
+	t_mutex	*mutex;
+}		t_all;
 /*
  stop = 0 - ta de boa
  stop = 1 - algum filosofo morreu
@@ -55,11 +66,15 @@ int	ft_atoi(char *s);
 int	all_checks(int argc, char **argv);
 int	check_positive_number(char **argv);
 int	check_bigger_than_zero(char **argv);
+t_all	*init_all(int argc, char **argv);
 t_philo	*init_philo(int argc, char **argv);
 t_philo	*init_parameters(int argc, char **argv, t_philo *philo, int total);
-t_mutex	*init_mutex(void);
-void	destroy_mutex(t_mutex *mutex);
+t_mutex	*init_mutex(int total);
+int	init_threads(t_all *all);
+void	destroy_mutex(int total, t_mutex *mutex);
 void	destroy_philo(t_philo *ph);
 long int	get_time(void);
+int	print(t_philo ph, t_mutex *mutex, int action);
+void	*routine(void *all);
 
 #endif
