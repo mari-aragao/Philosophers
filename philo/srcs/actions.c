e7fda6b15e1bdd2f5fede4974e4edce85ee20e28
mari-aragao <mariazevedoaragao@gmail.com>
2023-01-03 13:25:25 -0300

    update actions

--- philo/srcs/actions.c
+++ philo/srcs/actions.c
@@ -3,26 +3,23 @@
 int	take_forks(t_philo *ph)
 {
 	int total;
+	int fork_one;
+	int fork_two;
 
 	total = ph->vars->total;
-	if ((ph->id % 2) == 0 && ph->vars->arr_fk[ph->id % total] == 0 && ph->vars->arr_fk[(ph->id % total) + 1] == 0)
+	fork_one = ph->id % total;
+	if ((ph->id % 2) == 0)
+		fork_two = ph->id % total + 1;
+	else if ((ph->id % 2) == 1)
+		fork_two = ph->id % total - 1;
+	if (ph->vars->arr_fk[fork_one] == 0 && ph->vars->arr_fk[fork_two] == 0)
 	{
-		pthread_mutex_lock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_lock(&ph->vars->forks[(ph->id % total) + 1]);
-		ph->vars->arr_fk[ph->id % total] = 1;
-		ph->vars->arr_fk[(ph->id % total) + 1] = 1;
-		pthread_mutex_unlock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_unlock(&ph->vars->forks[(ph->id % total) + 1]);
-		return (0);
-	}
-	else if ((ph->id % 2) == 1 && ph->vars->arr_fk[ph->id % total] == 0 && ph->vars->arr_fk[(ph->id) % total - 1] == 0)
-	{
-		pthread_mutex_lock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_lock(&ph->vars->forks[(ph->id % total) - 1]);
-		ph->vars->arr_fk[ph->id % total] = 1;
-		ph->vars->arr_fk[(ph->id % total) - 1] = 1;
-		pthread_mutex_unlock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_unlock(&ph->vars->forks[(ph->id % total) - 1]);
+		pthread_mutex_lock(&ph->vars->forks[fork_one]);
+		pthread_mutex_lock(&ph->vars->forks[fork_two]);
+		ph->vars->arr_fk[fork_one] = 1;
+		ph->vars->arr_fk[fork_two] = 1;
+		pthread_mutex_unlock(&ph->vars->forks[fork_one]);
+		pthread_mutex_unlock(&ph->vars->forks[fork_two]);
 		return (0);
 	}
 	return (-1);
@@ -56,24 +53,19 @@ int	is_dead(t_philo *ph)
 void	drop_forks(t_philo *ph)
 {
 	int total;
+	int fork_one;
+	int fork_two;
 
 	total = ph->vars->total;
+	fork_one = ph->id % total;
 	if ((ph->id % 2) == 0)
-	{
-		pthread_mutex_lock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_lock(&ph->vars->forks[(ph->id % total) + 1]);
-		ph->vars->arr_fk[ph->id % total] = 0;
-		ph->vars->arr_fk[(ph->id % total) + 1] = 0;
-		pthread_mutex_unlock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_unlock(&ph->vars->forks[(ph->id % total) + 1]);
-	}
+		fork_two = ph->id % total + 1;
 	else if ((ph->id % 2) == 1)
-	{
-		pthread_mutex_lock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_lock(&ph->vars->forks[(ph->id % total) - 1]);
-		ph->vars->arr_fk[ph->id % total] = 0;
-		ph->vars->arr_fk[(ph->id % total) - 1] = 0;
-		pthread_mutex_unlock(&ph->vars->forks[ph->id % total]);
-		pthread_mutex_unlock(&ph->vars->forks[(ph->id % total) - 1]);
-	}
+		fork_two = ph->id % total - 1;
+	pthread_mutex_lock(&ph->vars->forks[fork_one]);
+	pthread_mutex_lock(&ph->vars->forks[fork_two]);
+	ph->vars->arr_fk[fork_one] = 0;
+	ph->vars->arr_fk[fork_two] = 0;
+	pthread_mutex_unlock(&ph->vars->forks[fork_one]);
+	pthread_mutex_unlock(&ph->vars->forks[fork_two]);
 }
